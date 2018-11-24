d46f5683614671d8faff8e9405095b4908fb695d
zhaoliangshan <995529442@qq.com>
2018-11-24 14:47:01 +0800

    33

--- server/http_server.php
+++ server/http_server.php
@@ -7,9 +7,15 @@ $http_server->set(
     [
         'enable_static_handler' => true,
         'document_root' => '/data/wwwroot/swoole_demo/public/static',
+        'worker_num'=> 4,
     ]
 );
  
+ $http_server->on('WorkerStart',function($http_server,$worker_id){
+	// 定义应用目录
+	define('APP_PATH', __DIR__ . '/../application/');
+	require __DIR__ . '/../thinkphp/base.php';
+ });
 $http_server->on('request',function($request ,$response){
     //设置响应头信息
     //$response->cookie('xyj','hello',86400);
