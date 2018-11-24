79b9b9d5842046d530b0a71ddca4c7734855b5ad
zhaoliangshan <995529442@qq.com>
2018-11-24 17:26:33 +0800

    444

--- server/http_server.php
+++ server/http_server.php
@@ -16,7 +16,7 @@ $http_server->set(
 	define('APP_PATH', __DIR__ . '/../application/');
 	require __DIR__ . '/../thinkphp/base.php';
  });
-$http_server->on('request',function($request ,$response){  
+$http_server->on('request',function($request ,$response) use ($http_server){  
      if(isset($request->server)){
      	foreach($request->server as $k=>$v){
      		$_SERVER[strtoupper($k)] = $v;
@@ -55,6 +55,8 @@ $http_server->on('request',function($request ,$response){
      ob_end_clean();
     //服务器返回信息
     $response->end($res);
+
+    $http_server->close();
 });
  
 $http_server->start();
