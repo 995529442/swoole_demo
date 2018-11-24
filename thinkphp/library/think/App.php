662fd251969f037dabbd1ae888f8fcfc5429f383
zhaoliangshan <995529442@qq.com>
2018-11-24 16:51:36 +0800

    444

--- thinkphp/library/think/App.php
+++ thinkphp/library/think/App.php
@@ -616,7 +616,7 @@ class App
      */
     public static function routeCheck($request, array $config)
     {
-        $path   = $request->path();echo $path;
+        $path   = $request->path();
         $depr   = $config['pathinfo_depr'];
         $result = false;
 
