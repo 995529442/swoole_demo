b97ca3831a74ec048031d5bee62ccb875483b544
zhaoliangshan <995529442@qq.com>
2018-11-26 09:39:39 +0800

    444

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -8,8 +8,24 @@ class Index
         var_dump($_GET);
     }
 
-   public function test()
+   public function login()
     {
-        echo "tests";
+    	$return = array(
+           "errcode" => -1,
+           "errmsg" => "fail"
+    	);
+        $code = mt_rand(1000,9999);
+
+        $redis = swoole_redis;
+
+        $redis = redis->connect('127.0.0.1',6379,function(swoole_redis $redis,$result){
+             $redis->set("phone",$code,function(swoole_redis $redis,$result){
+                 $return['errcode'] = 1;
+                 $return['errmsg'] = "success";
+             });
+
+             $redis->close();
+        });
+        echo $return;
     }
 }
