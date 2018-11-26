3e3e4377b7cad8be5645ed950c130bb2b321773b
sam <995529442@qq.com>
2018-11-26 11:53:14 +0800

    3

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -14,9 +14,31 @@ class Index
         var_dump($_GET);
     }
 
-   public function test()
+   public function checkLogin()
     {
-        echo "tests4444444";
+       $phone = $_GET['phone_num'];
+	   $code = $_GET['code'];
+	   
+	    $return = array(
+           "errcode" => -1,
+           "errmsg" => "fail"
+    	);
+		
+	   $redis = new \Redis();
+	   $redis->connect("127.0.0.1",6379);
+	   
+	   $old_num = $redis->get("sms_".$phone);
+	   
+	   if(!$old_num){
+		   $return['errmsg'] = "验证码已过期";
+	   }elseif($old_num != $code){
+		   $return['errmsg'] = "验证码错误";
+	   }else{
+	        $return['errcode'] = 1;
+        	$return['errmsg'] = "success";
+	   }
+	   
+	   return json_encode($return);
     }
 
    public function login()
